44e6a8816d2f3dc4536feab7329e870f9314de52
Changwoo <ep6tri@hotmail.com>
2021-09-23 11:39:37 +0900

    Code update

--- diary-helper.php
+++ diary-helper.php
@@ -15,7 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 if ( ! function_exists( 'diary_helper_get_client_ip_address' ) ) {
 	function diary_helper_get_client_ip_address(): string {
-		return $_SERVER['REMOTE_HOST'] ?? '';
+		return $_SERVER['REMOTE_ADDR'] ?? '';
 	}
 }
 
@@ -23,7 +23,7 @@ if ( ! function_exists( 'diary_helper_get_client_ip_address' ) ) {
 if ( ! function_exists( 'diary_helper_get_whitelist' ) ) {
 	function diary_helper_get_whitelist(): array {
 		if ( ! defined( 'DIARY_HELPER_WHITELIST' ) ) {
-			define( 'DIARY_HELPER_WHITELIST', '192.168.10.1' );
+			define( 'DIARY_HELPER_WHITELIST', '127.0.0.1, 192.168.10.1' );
 		}
 
 		return array_map( 'trim', explode( ',', DIARY_HELPER_WHITELIST ) );
@@ -33,7 +33,10 @@ if ( ! function_exists( 'diary_helper_get_whitelist' ) ) {
 
 if ( ! function_exists( 'diary_helper_is_protected' ) ) {
 	function diary_helper_is_protected(): bool {
-		$in_whitelist = in_array( diary_helper_get_client_ip_address(), diary_helper_get_whitelist(), true );
+		$ip_address = diary_helper_get_client_ip_address();
+		$whitelist  = diary_helper_get_whitelist();
+
+		$in_whitelist = in_array( $ip_address, $whitelist, true );
 		$is_logged_in = is_user_logged_in();
 
 		return ! ( $in_whitelist || $is_logged_in );
